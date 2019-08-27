5f6809bceb230b3e4a4ba2955242caa093e00e1d
katakurineko <>
2019-08-27 23:22:29 +0900

    アップロードできるように

--- src/main/java/jp/co/mediaseek/training/controller/MeishiRegisterPictureController.java
+++ src/main/java/jp/co/mediaseek/training/controller/MeishiRegisterPictureController.java
@@ -1,11 +1,18 @@
 package jp.co.mediaseek.training.controller;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import jp.co.mediaseek.training.exception.StorageException;
 
 @Controller
 public class MeishiRegisterPictureController {
@@ -18,8 +25,30 @@ public class MeishiRegisterPictureController {
   @PostMapping("/meishiRegisterPicture")
   public String handleFileUpload(@RequestParam("file") MultipartFile file) {
 
-    // storageService.store(file);
-    System.out.println(file);
+    String filename = StringUtils.cleanPath(file.getOriginalFilename());
+    System.out.println("filename:"+filename);
+    
+    Path rootLocation = Paths.get("src/main/webapp/Picture");
+    System.out.println(rootLocation);
+    
+    try {
+      if (file.isEmpty()) {
+          throw new StorageException("Failed to store empty file " + filename);
+      }
+      if (filename.contains("..")) {
+          // This is a security check
+          throw new StorageException(
+                  "Cannot store file with relative path outside current directory "
+                          + filename);
+      }
+      try (InputStream inputStream = file.getInputStream()) {
+          Files.copy(inputStream, rootLocation.resolve(filename),
+              StandardCopyOption.REPLACE_EXISTING);
+      }
+  }
+  catch (IOException e) {
+      throw new StorageException("Failed to store file " + filename, e);
+  }
 
     return "meishiEdit";
   }
